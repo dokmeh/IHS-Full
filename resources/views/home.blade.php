3935dfca70c814c22b46970c57a443124ee80565
Katerou22 <xkaterou22@gmail.com>
2016-12-28 14:57:35 +0330

    Again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,9 @@
             <h2>HEALTH SYSTEM</h2>
         </div>
         <div class="enter-links">
-            <a href="about" class="enter-link ba-line">About</a>
-            <a href="projects" class="enter-link ba-line">Projects</a>
-            <a href="awards" class="enter-link ba-line">Awards</a>
-            <a href="contact" class="enter-link ba-line">Contact Us</a>
+            <a href="/about" class="enter-link ba-line">About</a>
+            <a href="/projects" class="enter-link ba-line">Projects</a>
+            <a href="/awards" class="enter-link ba-line">Awards</a>
+            <a href="/contact" class="enter-link ba-line">Contact Us</a>
         </div>
     </div>
